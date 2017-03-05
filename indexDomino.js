036e9e73bc3c7ad603f666630d1469d835c84923
Roger Pedrós Villorbina <pedrooos.46@gmail.com>
2017-03-05 18:29:26 +0100

    Petits canvis.

--- indexDomino.js
+++ indexDomino.js
@@ -1,21 +1,7 @@
 /**
- * Created by pedro on 20/02/2017.
+ * Created by Roger Pedros on 20/02/2017.
  */
-/*
- * arxiu principal que arrenca el servidor HTTP
- * @author  sergi.grau@fje.edu
- * @version 1.0
- * date 31.1.2017
- * format del document UTF-8
- *
- * CHANGELOG
- * 31.1.2017
- * - arxiu principal que arrenca el servidor HTTP
- *
- * NOTES
- * ORIGEN
- * Desenvolupament Aplicacions Web. Jesuïtes el Clot
- */
+
 var server = require("./servidorDomino");
 
 server.iniciar();
--- js/logicaDomino.js
+++ js/logicaDomino.js
@@ -1,16 +1,10 @@
 /*
  * programa que permet tenir un xat amb AJAX
- * @author sergi.grau@fje.edu
+ * @author sergi.grau@fje.edu, Roger Pedros Villorbina
  * @version 1.0
  * date 13.02.2017
  * format del document UTF-8
- *
- * CHANGELOG
- * 13.02.2017
- * - programa  permet tenir un xat amb AJAX
- * NOTES
- * ORIGEN
- * Desenvolupament en entorn client. Escola del clot
+
  */
 
 var fitxesJugador = [];
@@ -44,90 +38,94 @@ function assignarFitxes(){
   console.log(fitxesJugador);
 }
 
+function getImagePath(number){
+  switch (number){
+    case "00": return "00.png"; break;
+
+    case "01":
+    case "10": return "10.png"; break;
+
+    case "11": return "11.png"; break;
+
+    case "02":
+    case "20": return "20.png"; break;
+
+    case "21":
+    case "12": return "21.png"; break;
+
+    case "22": return "22.png"; break;
+
+    case "03":
+    case "30": return "30.png"; break;
+
+    case "13":
+    case "31": return "31.png"; break;
+
+    case "23":
+    case "32": return "32.png"; break;
+
+    case "33": return "33.png"; break;
+
+    case "04":
+    case "40": return "40.png"; break;
+
+    case "14":
+    case "41": return "41.png"; break;
+
+    case "24":
+    case "42": return "42.png"; break;
+
+    case "34":
+    case "43": return "42.png"; break;
+
+    case "44": return "42.png"; break;
+
+    case "05":
+    case "50": return "50.png"; break;
+
+    case "15":
+    case "51": return "51.png"; break;
+
+    case "25":
+    case "52": return "52.png"; break;
+
+    case "35":
+    case "53": return "53.png"; break;
+
+    case "45":
+    case "54": return "54.png"; break;
+
+    case "55": return "55.png"; break;
+
+    case "06":
+    case "60": return "60.png"; break;
+
+    case "16":
+    case "61": return "61.png"; break;
+
+    case "26":
+    case "62": return "62.png"; break;
+
+    case "36":
+    case "63": return "63.png"; break;
+
+    case "46":
+    case "64": return "46.png"; break;
+
+    case "56":
+    case "65": return "65.png"; break;
+
+    case "66": return "66.png"; break;
+
+  }
+}
+
 
 function printarFitxes(){
   var i=1;
   for(key in fitxesJugador){
-    imageUrl = "";
-    switch (fitxesJugador[key].toString()){
-      case "00": imageUrl = "00.png"; break;
-
-      case "01":
-      case "10": imageUrl = "10.png"; break;
-
-      case "11": imageUrl = "11.png"; break;
-
-      case "02":
-      case "20": imageUrl = "20.png"; break;
-
-      case "21":
-      case "12": imageUrl = "21.png"; break;
-
-      case "22": imageUrl = "22.png"; break;
-
-      case "03":
-      case "30": imageUrl = "30.png"; break;
-
-      case "13":
-      case "31": imageUrl = "31.png"; break;
-
-      case "23":
-      case "32": imageUrl = "32.png"; break;
-
-      case "33": imageUrl = "33.png"; break;
-
-      case "04":
-      case "40": imageUrl = "40.png"; break;
-
-      case "14":
-      case "41": imageUrl = "41.png"; break;
-
-      case "24":
-      case "42": imageUrl = "42.png"; break;
-
-      case "34":
-      case "43": imageUrl = "42.png"; break;
-
-      case "44": imageUrl = "42.png"; break;
-
-      case "05":
-      case "50": imageUrl = "50.png"; break;
-
-      case "15":
-      case "51": imageUrl = "51.png"; break;
-
-      case "25":
-      case "52": imageUrl = "52.png"; break;
-
-      case "35":
-      case "53": imageUrl = "53.png"; break;
-
-      case "45":
-      case "54": imageUrl = "54.png"; break;
-
-      case "55": imageUrl = "55.png"; break;
-
-      case "06":
-      case "60": imageUrl = "60.png"; break;
-
-      case "16":
-      case "61": imageUrl = "61.png"; break;
-
-      case "26":
-      case "62": imageUrl = "62.png"; break;
-
-      case "36":
-      case "63": imageUrl = "63.png"; break;
-
-      case "46":
-      case "64": imageUrl = "46.png"; break;
-
-      case "56":
-      case "65": imageUrl = "65.png"; break;
-
-      case "66": imageUrl = "66.png"; break;
-
-    }
+    imageUrl =  getImagePath(fitxesJugador[key].toString());
+    //getImagePath(fitxesJugador[key].toString());
 
     var x = document.createElement("img");
     x.setAttribute("src", "imatges/" + imageUrl);
@@ -207,86 +205,8 @@ window.onload = () => {
                 if (resposta != darreraResposta) {
                     console.log('Rebut per lajax:',resposta);
 
-                    imageUrl= "";
-                    switch (resposta.toString()){
-                      case "00": imageUrl = "00.png"; break;
+                    imageUrl= getImagePath(resposta.toString());
 
-                      case "01":
-                      case "10": imageUrl = "10.png"; break;
-
-                      case "11": imageUrl = "11.png"; break;
-
-                      case "02":
-                      case "20": imageUrl = "20.png"; break;
-
-                      case "21":
-                      case "12": imageUrl = "21.png"; break;
-
-                      case "22": imageUrl = "22.png"; break;
-
-                      case "03":
-                      case "30": imageUrl = "30.png"; break;
-
-                      case "13":
-                      case "31": imageUrl = "31.png"; break;
-
-                      case "23":
-                      case "32": imageUrl = "32.png"; break;
-
-                      case "33": imageUrl = "33.png"; break;
-
-                      case "04":
-                      case "40": imageUrl = "40.png"; break;
-
-                      case "14":
-                      case "41": imageUrl = "41.png"; break;
-
-                      case "24":
-                      case "42": imageUrl = "42.png"; break;
-
-                      case "34":
-                      case "43": imageUrl = "42.png"; break;
-
-                      case "44": imageUrl = "42.png"; break;
-
-                      case "05":
-                      case "50": imageUrl = "50.png"; break;
-
-                      case "15":
-                      case "51": imageUrl = "51.png"; break;
-
-                      case "25":
-                      case "52": imageUrl = "52.png"; break;
-
-                      case "35":
-                      case "53": imageUrl = "53.png"; break;
-
-                      case "45":
-                      case "54": imageUrl = "54.png"; break;
-
-                      case "55": imageUrl = "55.png"; break;
-
-                      case "06":
-                      case "60": imageUrl = "60.png"; break;
-
-                      case "16":
-                      case "61": imageUrl = "61.png"; break;
-
-                      case "26":
-                      case "62": imageUrl = "62.png"; break;
-
-                      case "36":
-                      case "63": imageUrl = "63.png"; break;
-
-                      case "46":
-                      case "64": imageUrl = "46.png"; break;
-
-                      case "56":
-                      case "65": imageUrl = "65.png"; break;
-
-                      case "66": imageUrl = "66.png"; break;
-
-                    }
                     li.innerHTML = "<img src='imatges/" + imageUrl+ "'>";
                     //document.write("<img src='imatges/" + imageUrl+ "'>");
                     darreraResposta = resposta;
